5adbda4e8eb0aadde3de273f1029508878637fd4
MahmoudAlshehawy <87283364+MahmoudAlshehawy@users.noreply.github.com>
2021-11-29 00:40:18 +0200

    Add files via upload

---
+++ UART_CONFIG.h
@@ -0,0 +1,29 @@
+/*
+* UART_CONFIG.h
+*
+* Created: 2/13/2021 9:10:53 PM
+*  Author: karim
+*/
+
+
+#ifndef UART_CONFIG_H_
+#define UART_CONFIG_H_
+
+#include "CPU_Configurations.h"
+/*
+#define UBRRL (*(volatile Uint8t*)(0x29))
+#define UBRRH (*(volatile Uint8t*)(0x40))
+#define UCSRA (*(volatile Uint8t*)(0x2B))
+#define UCSRB (*(volatile Uint8t*)(0x29A)
+#define UCSRC (*(volatile Uint8t*)(0x40))
+#define UDR   (*(volatile Uint8t*)(0x2C))
+*/
+#define FRQ      160000
+#define BAUDRATE 96
+typedef enum{
+	UART_NOk =0,
+	UART_OK =1
+}UART_STATUS;
+
+
+#endif /* UART_CONFIG_H_ */
